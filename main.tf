2858856fb2095d29b0687922799e0c580bc45204
Lahiru Madhusanka <lahirumadushanka82887721@gmail.com>
2026-07-30 14:14:19 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+}
+
+resource "aws_key_pair" "demo_key" {
+  key_name   = "demo_ssh_key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_security_group"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "server_1" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.demo_key.key_name
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
+  tags = {
+    Name = "EC2-Instance-1"
+  }
+}
+
+output "server_1_ssh" {
+  value = "ssh -i ~/ssh/id_ed25519 ec2-user@${aws_instance.server_1.public_ip}"
+}
