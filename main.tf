0160ddd306c20a28f32200250770b1aaa704c63c
Lahiru Madhusanka <lahirumadushanka82887721@gmail.com>
2026-07-30 16:04:12 +0530

    Add security group ingress rules and new EC2 instances with outputs

--- main.tf
+++ main.tf
@@ -37,7 +37,13 @@ resource "aws_security_group" "demo_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ingress {
+   description = "HTTP access"
+   from_port = 80
+   to_port = 80
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+}
   egress {
     from_port   = 0
     to_port     = 0
@@ -58,5 +64,34 @@ resource "aws_instance" "server_1" {
 }
 
 output "server_1_ssh" {
+  value = "ssh -i ~/ssh/id_ed25519 ec2-user@${aws_instance.server_1.public_ip}"
+}
+
+resource "aws_instance" "server_2" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.demo_key.key_name
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
+  tags = {
+    Name = "EC2-Instance-2"
+  }
+}
+
+output "server_2_ssh" {
+  value = "ssh -i ~/ssh/id_ed25519 ec2-user@${aws_instance.server_1.public_ip}"
+}
+resource "aws_instance" "server_3" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.demo_key.key_name
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
+  tags = {
+    Name = "EC2-Instance-3"
+  }
+}
+
+output "server_3_ssh" {
   value = "ssh -i ~/ssh/id_ed25519 ec2-user@${aws_instance.server_1.public_ip}"
 }
